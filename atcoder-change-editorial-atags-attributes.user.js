2bad8577918acfcdb75f2ee1668e583e00889032
ilplrr <ilplrr193@gmail.com>
2021-09-19 04:38:35 +0900

    Initial commit.

---
+++ atcoder-change-editorial-atags-attributes.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         atcoder-change-editorial-atags-attributes
+// @namespace    https://github.com/ilplrr
+// @version      1.0
+// @description  問題ページの「解説」を新しいタブで開くように設定します。また、解説ページのリンクを同じタブで開くよう設定します。
+// @author       ilplrr
+// @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/tasks\/[^\/]+$/
+// @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/tasks\/[^\/]+\/editorial$/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const basename = location.href.split('/').pop();
+    if (basename === 'editorial') {
+        document.querySelectorAll('a').forEach(atag => {
+            if(['ユーザ解説','公式解説'].includes(atag.textContent)) {
+                atag.removeAttribute('target');
+            }
+        });
+    }else{
+        const elm = document.querySelector(`a[href="${location.pathname}/editorial"]`);
+        elm.setAttribute('target','_blank');
+        elm.setAttribute('rel','noopener');
+    }
+})();
