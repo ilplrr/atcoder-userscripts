7f77b5a033f989e5f7e4b1333e9b14d44a7172c4
ilplrr <ilplrr193@gmail.com>
2021-11-14 01:06:18 +0900

    update: 実行タイミングを CSS が描画される前に変更

--- atcoder-remove-unusual-css.user.js
+++ atcoder-remove-unusual-css.user.js
@@ -1,17 +1,21 @@
 // ==UserScript==
 // @name         atcoder-remove-unusual-css
 // @namespace    https://github.com/ilplrr
-// @version      1.0
+// @version      1.1
 // @description  企業コンなどの普段と違う色設定を削除します。
 // @author       ilplrr
 // @match        https://atcoder.jp/contests/*
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
+    document.addEventListener('readystatechange' ,() => {
+        if (document.readyState !== 'interactive') return;
 
-    // 1 番目の style タグに書かれているっぽい。
-    // この style タグは企業コンなどでなくても置かれている（普段はタグ内テキストが空文字列）。
-    document.querySelector('style').textContent = '';
+        // 1 番目の style タグに書かれているっぽい。
+        // この style タグは企業コンなどでなくても置かれている（普段はタグ内テキストが空文字列）。
+        document.querySelector('style').textContent = '';
+    });
 })();
