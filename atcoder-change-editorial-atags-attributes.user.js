5dfdbbe2acb006dc787cf5f60543018771354a16
ilplrr <46692443+ilplrr@users.noreply.github.com>
2024-10-16 02:22:18 +0900

    update

--- atcoder-change-editorial-atags-attributes.user.js
+++ atcoder-change-editorial-atags-attributes.user.js
@@ -1,28 +1,31 @@
 // ==UserScript==
 // @name         atcoder-change-editorial-atags-attributes
 // @namespace    https://github.com/ilplrr
-// @version      1.2
-// @description  問題ページの「解説」を新しいタブで開くように設定します。また、解説ページのリンクを同じタブで開くよう設定します。
+// @version      1.3
+// @description  問題ページの「解説」を新しいタブで開くように設定します。また、解説ページ内のリンク（外部サイト除く）を同じタブで開くよう設定します。
 // @author       ilplrr
 // @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/tasks\/[^\/]+$/
-// @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/tasks\/[^\/]+\/editorial$/
-// @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/editorial$/
+// @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/tasks\/[^\/]+\/editorial\/?$/
+// @include      /^https:\/\/atcoder\.jp\/contests\/[^\/]+\/editorial\/?$/
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    const basename = location.href.split('/').pop();
-    if (basename === 'editorial') {
-        document.querySelectorAll('a').forEach(atag => {
-            if (atag.href.indexOf('/editorial/') > 0) {
-                atag.removeAttribute('target');
-            }
-        });
-    }else{
-        const elm = document.querySelector(`a[href="${location.pathname}/editorial"]`);
-        elm.setAttribute('target','_blank');
-        elm.setAttribute('rel','noopener');
-    }
+  const m = location.pathname.match(/^\/contests\/(?<contestId>[^\/]+)/);
+  const contestPathname = m ? m[0] : null;
+
+  if (location.href.match(/\/editorial\/?$/)) {
+    const editorialPathname = `${contestPathname}/editorial/`;
+    document.querySelectorAll(`a[href^="${editorialPathname}"]`).forEach((a) => {
+      if (a.pathname.startsWith(editorialPathname) && a.pathname !== editorialPathname) {
+        a.removeAttribute('target');
+      }
+    });
+  } else {
+    const elm = document.querySelector(`a[href="${location.pathname}/editorial"]`);
+    elm.setAttribute('target', '_blank');
+    elm.setAttribute('rel', 'noopener');
+  }
 })();
