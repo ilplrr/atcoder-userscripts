4c1adcf5e8f5c1af889654af61d532af18303094
ilplrr <46692443+ilplrr@users.noreply.github.com>
2024-12-18 11:50:18 +0900

    update

--- atcoder-remove-unusual-css.user.js
+++ atcoder-remove-unusual-css.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         atcoder-remove-unusual-css
 // @namespace    https://github.com/ilplrr
-// @version      1.1
+// @version      1.2
 // @description  企業コンなどの普段と違う色設定を削除します。
 // @author       ilplrr
 // @match        https://atcoder.jp/contests/*
@@ -9,13 +9,16 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    document.addEventListener('readystatechange' ,() => {
-        if (document.readyState !== 'interactive') return;
+(function () {
+  'use strict';
 
-        // 1 番目の style タグに書かれているっぽい。
-        // この style タグは企業コンなどでなくても置かれている（普段はタグ内テキストが空文字列）。
-        document.querySelector('style').textContent = '';
-    });
+  function removeUnusualCss() {
+    // 1 番目の style タグに書かれているっぽい。
+    // この style タグは企業コンなどでなくても置かれている（普段は要素内テキストが空文字列）。
+    document.querySelector('style').textContent = '';
+  }
+
+  document.addEventListener('readystatechange', removeUnusualCss);
+  document.addEventListener('DOMContentLoaded', removeUnusualCss);
+  window.addEventListener('load', removeUnusualCss);
 })();
